c226f859e1fc31b896c269d48ef8a721214871bd
ksy <tpdud3256@gmail.com>
2023-03-18 06:25:59 +0900

    작업진행...

--- src/services/works/getMyAcceptList.js
+++ src/services/works/getMyAcceptList.js
@@ -33,11 +33,21 @@ export const getMyAcceptList = async (req, res) => {
             orderBy: { id: "desc" },
         });
 
-        console.log(list);
+        const result = [];
 
+        if (list.length > 0) {
+            list.map((order, index) => {
+                const workDateTime = new Date(workDateTime);
+                const today = new Date();
+                if (workDateTime < today) {
+                    result.push(order);
+                }
+            });
+        }
+        console.log(result);
         if (!list) throw new Error("작업 불러오기에 실패하였습니다.");
 
-        res.json(setResponseJson({ list: list }));
+        res.json(setResponseJson({ list: result }));
     } catch (error) {
         res.json(setErrorJson(error.message));
     }
