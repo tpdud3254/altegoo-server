584e5d9bd6cae34f831ee25ae9ef402eda24f8e9
tpdud3254 <tpdud3254@burninggalaxy.com>
2023-04-24 16:46:20 +0900

    작업 진행 api 작업 중

--- src/routes/works/works.js
+++ src/routes/works/works.js
@@ -10,6 +10,7 @@ import { getMyWorkList } from "../../services/works/getMyWorkList";
 import { getMyAcceptList } from "../../services/works/getMyAcceptList";
 import { getMyRegistList } from "../../services/works/getMyRegistList";
 import { getWorkInfo } from "../../services/works/getWorkInfo";
+import { acceptOrder } from "../../services/works/acceptOrder";
 
 const worksRouter = express.Router();
 
@@ -26,5 +27,19 @@ worksRouter.post("/reservation", auth, asyncWrap(addReservation));
 worksRouter.delete("/reservation", auth, asyncWrap(deleteReservation));
 
 worksRouter.patch("/status", auth, asyncWrap(setWorkStatus));
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+//작업예약
+worksRouter.patch("/order/accept", auth, asyncWrap(acceptOrder));
+
+//작업취소
+//예약취소
+//예약대기
+//예약대기취소
+//작업출발
+//작업시작
+//작업완료
+//작업종료
 
 export default worksRouter;
---
+++ src/services/works/acceptOrder.js
@@ -0,0 +1,63 @@
+import prisma from "../../prisma";
+import { setErrorJson, setResponseJson } from "../../utils";
+
+export const acceptOrder = async (req, res) => {
+  const { id: orderId } = req.body;
+  const id = req.id;
+
+  if (!orderId || !id) throw new Error("작업 상태를 변경할 수 없습니다.");
+
+  try {
+    const order = await prisma.order.findUnique({
+      where: {
+        id: orderId,
+      },
+      select: {
+        orderStatusId,
+      },
+    });
+
+    if (order.orderStatusId === 2) throw new Error("이미 예약된 작업");
+
+    const updatedOrder = await prisma.order.update({
+      where: {
+        id: orderId,
+      },
+      data: {
+        acceptUser: id,
+        status: {
+          connect: {
+            id: 2,
+          },
+        },
+      },
+      include: {
+        registUser: { select: { id: true } },
+        orderReservation: true,
+      },
+    });
+
+    if (!updatedOrder) throw new Error("작업상태 변경에 실패했습니다.");
+
+    const workList = await prisma.order.findMany({
+      include: {
+        registUser: { select: { id: true } },
+        orderReservation: true,
+      },
+      orderBy: {
+        id: "desc",
+      },
+      // take: 5,
+      // skip: lastUserId ? 1 : 0,
+      // ...(lastUserId && { cursor: { id: lastUserId } }),
+      //TODO: pagination
+    });
+
+    if (!workList) throw new Error("작업상태 변경에 실패했습니다.");
+
+    //TODO: 리스트 반환,.,?
+    res.json(setResponseJson({ list: workList }));
+  } catch (error) {
+    res.json(setErrorJson(error.message));
+  }
+};
