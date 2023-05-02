4f755c4822af08e05704355823b8a4c1a648c42b
tpdud3254 <tpdud3254@burninggalaxy.com>
2023-05-02 17:29:30 +0900

    작업진행 로직 수정

--- src/services/works/getMyRegistList.js
+++ src/services/works/getMyRegistList.js
@@ -2,38 +2,38 @@ import prisma from "../../prisma";
 import { setErrorJson, setResponseJson } from "../../utils";
 
 export const getMyRegistList = async (req, res) => {
-    const id = req.id;
+  const id = req.id;
 
-    try {
-        const list = await prisma.order.findMany({
-            where: {
-                OR: [
-                    {
-                        AND: [
-                            {
-                                registUser: {
-                                    id,
-                                },
-                            },
-                            {
-                                orderStatusId: 4,
-                            },
-                        ],
-                    },
-                ],
-            },
-            include: {
-                registUser: { select: { id: true } },
-                orderReservation: true,
-            },
-            orderBy: { id: "desc" },
-        });
+  try {
+    const list = await prisma.order.findMany({
+      where: {
+        OR: [
+          {
+            AND: [
+              {
+                registUser: {
+                  id,
+                },
+              },
+              {
+                orderStatusId: 5,
+              },
+            ],
+          },
+        ],
+      },
+      include: {
+        registUser: { select: { id: true } },
+        orderReservation: true,
+      },
+      orderBy: { id: "desc" },
+    });
 
-        console.log(list);
-        if (!list) throw new Error("작업 불러오기에 실패하였습니다.");
+    console.log(list);
+    if (!list) throw new Error("작업 불러오기에 실패하였습니다.");
 
-        res.json(setResponseJson({ list: list }));
-    } catch (error) {
-        res.json(setErrorJson(error.message));
-    }
+    res.json(setResponseJson({ list: list }));
+  } catch (error) {
+    res.json(setErrorJson(error.message));
+  }
 };
