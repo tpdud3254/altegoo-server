738b5611ef0cde29cf8cedfd480f784b927cd5f8
seyoung Ko <tpdud3254@burninggalaxy.com>
2023-07-20 17:38:57 +0900

    getUserExist response 값 변경

--- src/services/users/getUserExist.js
+++ src/services/users/getUserExist.js
@@ -1,15 +1,21 @@
 import { existUser, setErrorJson, setResponseJson } from "../../utils";
 
 export const getUserExist = async (req, res) => {
-  const { phone } = req.query;
+    const { phone } = req.query;
 
-  console.log(phone);
-  //TODO 예외처리 보강강
-  try {
-    const user = await existUser(phone);
-    res.json(setResponseJson({ userId: user.id }));
-  } catch (error) {
-    console.log(error.message);
-    res.json(setErrorJson(error.message));
-  }
+    console.log(phone);
+    //TODO 예외처리 보강강
+    try {
+        const user = await existUser(phone);
+        res.json(
+            setResponseJson({
+                userId: user.id,
+                name: user.name,
+                phone: user.phone,
+            })
+        );
+    } catch (error) {
+        console.log(error.message);
+        res.json(setErrorJson(error.message));
+    }
 };
