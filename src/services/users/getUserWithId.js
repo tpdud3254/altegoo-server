debd515140e9b498358cf65cca29fdebf26ea8cb
seyoung Ko <tpdud3256@gmail.com>
2023-09-02 01:29:56 +0900

    getUserWithId api 수정

--- src/services/users/getUserWithId.js
+++ src/services/users/getUserWithId.js
@@ -4,19 +4,11 @@ import { existUser, setErrorJson, setResponseJson } from "../../utils";
 export const getUserWithId = async (req, res) => {
     const { id } = req.query;
 
+    console.log("getUserWithId id: ", id);
     //TODO 예외처리 보강강
     try {
         const user = await prisma.user.findUnique({
             where: { id: parseInt(id) },
-            include: {
-                vehicle: {
-                    include: {
-                        type: true,
-                        floor: true,
-                        weight: true,
-                    },
-                },
-            },
         });
         res.json(setResponseJson({ user: user }));
     } catch (error) {
