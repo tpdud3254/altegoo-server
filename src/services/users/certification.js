0243040e0170034ca03ae40bc65aad8fbb0a3adb
seyoung Ko <tpdud3256@gmail.com>
2023-09-02 23:19:31 +0900

    휴대폰 본인인증

--- src/services/users/certification.js
+++ src/services/users/certification.js
@@ -5,7 +5,7 @@ const NICE_SERVER = "https://svc.niceapi.co.kr:22001";
 const client_id = "2d981f65-0f61-4a27-b076-5ed681f30763";
 const client_secret = "c26a268437276d584bbc0361224ff79a";
 
-export const certification = () => {
+export const certification = (req, res) => {
     const getToken = async () => {
         const params = new URLSearchParams();
 
