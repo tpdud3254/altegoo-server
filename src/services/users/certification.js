bc14fcb60b93c13b9c8beda4a92a088f86e4d34d
seyoung Ko <tpdud3256@gmail.com>
2023-09-04 00:41:11 +0900

    휴대폰 본인인증

--- src/services/users/certification.js
+++ src/services/users/certification.js
@@ -67,7 +67,7 @@ export const certification = (req, res) => {
 
             console.log("hash : ", hash);
 
-            const key = hash.digest().slice(0, 16);
+            const key = hash.slice(0, 16);
             const iv = hash.substring(hash.length - 16, hash.length);
             const hmac_key = hash.substring(0, 32);
 
