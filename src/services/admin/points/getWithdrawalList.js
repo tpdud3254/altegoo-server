2fefb6a174f55fcb356dd03ba0981d38d33c505c
seyoung Ko <tpdud3256@gmail.com>
2023-12-21 15:43:06 +0900

    포인트 내역 로직 수정

--- src/services/admin/points/getWithdrawalList.js
+++ src/services/admin/points/getWithdrawalList.js
@@ -7,7 +7,7 @@ import {
 } from "../../../utils";
 
 const getWithdrawalList = async (req, res) => {
-    const { startDate, endDate, name, userId, phone } = req.query;
+    const { startDate, endDate, name, userId, phone, type } = req.query;
     console.log(req.query);
     try {
         const list = await prisma.pointBreakdown.findMany({
@@ -23,6 +23,7 @@ const getWithdrawalList = async (req, res) => {
                 ...(name && { user: { name } }),
                 ...(userId && { user: { id: Number(userId) } }),
                 ...(phone && { user: { phone } }),
+                ...(type && { type }),
             },
             include: {
                 user: {
@@ -35,79 +36,6 @@ const getWithdrawalList = async (req, res) => {
             },
             orderBy: { id: "desc" },
         });
-        // const users = await prisma.user.findMany({
-        //     where: {
-        //         ...(startDate &&
-        //             endDate && {
-        //                 AND: [
-        //                     { createdAt: { gt: GetPlusDateTime(startDate) } },
-        //                     { createdAt: { lt: GetPlusDateTime(endDate) } },
-        //                 ],
-        //             }),
-        //         ...(name && { name }),
-        //         ...(phone && { phone }),
-        //         ...(gender && { gender }),
-        //         ...(status && { status }),
-        //         ...(userTypeId && { userTypeId: Number(userTypeId) }),
-        //         ...(workCategoryId && {
-        //             workCategoryId: Number(workCategoryId),
-        //         }),
-        //         ...(region && {
-        //             accessedRegion: { contains: region },
-        //         }),
-        //     },
-        //     include: {
-        //         userType: true,
-        //         vehicle: true,
-        //         workRegion: true,
-        //         grade: true,
-        //         point: true,
-        //         pointBreakdown: true,
-        //         order: true,
-        //     },
-        //     orderBy: { id: "asc" },
-        // });
-
-        // if (!users) throw new Error("유저 리스트를 불러올 수 없습니다.");
-
-        // async function getRecommendUser() {
-        //     const usersList = await Promise.all(
-        //         users.map(async (user, index) => {
-        //             delete user.password;
-
-        //             if (user.recommendUserId) {
-        //                 if (user.recommendUserId !== 1) {
-        //                     const recommendUser = await prisma.user.findUnique({
-        //                         where: { id: user.recommendUserId },
-        //                         select: { name: true, id: true },
-        //                     });
-
-        //                     users[index] = {
-        //                         recommendUser: recommendUser,
-        //                         ...(await getUserRestInfo(user)),
-        //                         ...users[index],
-        //                     };
-        //                 } else {
-        //                     users[index] = {
-        //                         recommendUser: { id: 1, name: "알테구" },
-        //                         ...(await getUserRestInfo(user)),
-        //                         ...users[index],
-        //                     };
-        //                 }
-        //             } else {
-        //                 users[index] = {
-        //                     recommendUser: null,
-        //                     ...(await getUserRestInfo(user)),
-        //                     ...users[index],
-        //                 };
-        //             }
-        //         })
-        //     );
-        // }
-
-        // await getRecommendUser();
-
-        // console.log(users);
 
         res.json(setResponseJson({ list: list }));
     } catch (error) {
